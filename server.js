1f25ac5f53ec717c76085defd7c0a73727ac1498
silverdiva <silverdiva4u@gmail.com>
2019-04-01 23:41:20 -0700

    added Week 10 teach files for milestone 1 Project 2 testing

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require("express");
+require('dotenv').config();
+var path = require('path')
 
+
+//locahost
 var app = express();
 const PORT = process.env.PORT || 5000
+
+//localhost
+app.listen(PORT, function () {
+	console.log(`Listening on ${ PORT }`)
+});
 
 app.use(express.static("public"));
 
@@ -16,24 +25,113 @@ app.get("/", function (req, res) {
 app.get("/pages", function (req, res) {
 	// Controller
 	console.log("Received a request for the home page");
+	var params = {
+		result: getPerson(req.query)
+	};
 	res.render("pages");
 });
 
-app.get("/math", function (req, res) {
+
+
+//week 10 teach code + familyhistory endpoint callback  (using teacher solution functions)
+
+app.get("/familyhistory", function (req, res) {
 	// Controller
-	console.log("Received a request for the math page");
+	console.log("Received a request for the familyhistory page");
 	var params = {
-		result: getResults(req.query)
+		result:getPerson(req.query)
 	};
-	res.render("math", params);
+	res.render("familyhistory");
 });
+	// Following the "Single query" approach from: https://node-postgres.com/features/pooling#single-query
+	const { Pool } = require("pg"); // This is the postgres database connection module.
 
-app.get("/math_service", function (req, res) {
-	console.log("Received a request for the math_service page");
-	res.render("math_service");
-});
+	
+213258787\
+// LOCAL POSTGRES DB: connection string that refers to a local postgres DB - ONLY USE FOR TESTING PURPOSES
+		//const connectionString = process.env.DATABASE_URL || "postgres://admin_user:admin_pass@localhost:5432/familyhistory";
+	
+
+/*TOGGLE BETWEEN THE 2 CONNECTION STRINGS - ONE USES "true" and the other doesn't
+//i.e comment ONE of them out!*/
+//when connecting to HEROKU CLOUD APP, we need "true" at end of connectionString - //notice that "postgresql-metric-65706:" is used here
+
+const connectionString = process.env.DATABASE_URL;
+
+	//const connectionString = process.env.DATABASE_URL || "postgresql-metric-65706://nvjydtnfbwouem:df4e48d06bd2f5eb0306c6966cf1034810ad92855b79b464a3ec9e8a7cb9b888@ec2-23-21-106-241.compute-1.amazonaws.com:5432/d427tvjt5pb8vn="true";
 
 
+
+	// Establish a new connection to the data source specified the connection string.
+	const pool = new Pool({connectionString: connectionString});
+
+	// This says that we want the function "getPerson" below to handle
+	// any requests that come to the /getPerson endpoint
+	app.get("/getPerson", getPerson);
+
+	// This function handles requests to the /getPerson endpoint
+	// it expects to have an id on the query string, such as: http://localhost:5000/getPerson?id=1
+	function getPerson(request, response) {
+		// First get the person's id
+		const id = request.query.id;
+
+		// TODO: We should really check here for a valid id before continuing on...
+
+		// use a helper function to query the DB, and provide a callback for when it's done
+		getPersonFromDb(id, function(error, result) {
+			// This is the callback function that will be called when the DB is done.
+			// The job here is just to send it back.
+
+			// Make sure we got a row with the person, then prepare JSON to send back
+			if (error || result == null || result.length != 1) {
+				response.status(500).json({success: false, data: error});
+			} else {
+				const person = result[0];
+				response.status(200).json(person);
+			}
+		});
+	}
+
+	// This function gets a person from the DB.
+	// By separating this out from the handler above, we can keep our model
+	// logic (this function) separate from our controller logic (the getPerson function)
+	function getPersonFromDb(id, callback) {
+		console.log("Getting person from DB with id: " + id);
+
+		// Set up the SQL that we will use for our query. Note that we can make
+		// use of parameter placeholders just like with PHP's PDO.
+		const sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+
+		// We now set up an array of all the parameters we will pass to fill the
+		// placeholder spots we left in the query.
+		const params = [id];
+
+		// This runs the query, and then calls the provided anonymous callback function
+		// with the results.
+		pool.query(sql, params, function(err, result) {
+			// If an error occurred...
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+
+			// Log this to the console for debugging purposes.
+			console.log("Found result: " + JSON.stringify(result.rows));
+
+			// When someone else called this function, they supplied the function
+			// they wanted called when we were all done. Call that function now
+			// and pass it the results.
+
+			// (The first parameter is the error variable, so we will pass null.)
+			callback(null, result.rows);
+		});
+
+	} // end of getPersonFromDb
+//end of week 10 teach code + familyhistory endpoint callback(teacher solution)
+
+
+//week 9 PROVE callback + code
 app.get("/postal", function (req, res) {
 	// Controller
 	console.log("Received a request for the postal page");
@@ -42,102 +140,134 @@ app.get("/postal", function (req, res) {
 	};
 	res.render("postal", params);
 });
-
 app.get("/postal_service", function (req, res) {
 	console.log("Received a request for the postal_service page");
 	res.render("postal_service");
 });
-
-app.listen(PORT, function () {
-	console.log(`Listening on ${ PORT }`)
-});
-
-//week 9 teach code
-function getResults(data) {
-	if (data.submit == 'Add') {
-		return add(data.num1, data.num2);
-	} else if (data.submit == 'Subtract') {
-		return subtract(data.num1, data.num2);
-	} else if (data.submit == 'Multiply') {
-		return multiply(data.num1, data.num2);
-	} else if (data.submit == 'Divide') {
-		return divide(data.num1, data.num2);
-	} else {
-		console.log("Something weird happened. What did you click on????");
-	}
-}
-
-function subtract(num1, num2) {
-	// console.log("Subtract" + num1 + ' ' + num2);
-	return parseInt(num1) - parseInt(num2);
-}
-
-function divide(num1, num2) {
-	// console.log("Divide" + num1 + ' ' + num2);
-	return parseInt(num1) / parseInt(num2);
-}
-
-function multiply(num1, num2) {
-	// console.log("Multiply" + num1 + ' ' + num2);
-	return parseInt(num1) * parseInt(num2);
-}
-
-function add(num1, num2) {
-	// console.log("Add" + num1 + ' ' + num2);
-	return parseInt(num1) + parseInt(num2);
-}
-
-
-//week 9 PROVE code
 app.get("/calculateShipping", calculate);
-
-
-//week 9 prove code for postal rate calculator
-//not sure if I need to declare these or not
-/*const oz = 0;
-var type = null;
-*/
-
-function calculate(request, response) {
-	const lb = Number(request.query.lb);
-	const oz = Number(request.query.oz);
-	const type = request.query.type;
-
-	if (lb == 0 &&
-		oz > 4 && (type == "First Class Letter: Stamped" ||
-			type == "First Class Letter: Metered")) {
-		type = "Large Flat Envelope";
+	//result: getResults(req.query)
+	function getResults(data) {
+		if (data.submit == 'Add') {
+			return add(data.num1, data.num2);
+		} else if (data.submit == 'Subtract') {
+			return subtract(data.num1, data.num2);
+		} else if (data.submit == 'Multiply') {
+			return multiply(data.num1, data.num2);
+		} else if (data.submit == 'Divide') {
+			return divide(data.num1, data.num2);
+		} else {
+			console.log("Something weird happened. What did you click on????");
+		}
 	}
-	if (lb == 0 && oz > 13 && type == "First Class Envelopes: Large Flat") {
-		type = "First Class Package";
+	//app.get("/calculateShipping", calculate);
+	function calculate(request, response) {
+		const lb = Number(request.query.lb);
+		const oz = Number(request.query.oz);
+		const type = request.query.type;
+
+		if (lb == 0 &&
+			oz > 4 && (type == "First Class Letter: Stamped" ||
+				type == "First Class Letter: Metered")) {
+			type = "Large Flat Envelope";
+		}
+		if (lb == 0 && oz > 13 && type == "First Class Envelopes: Large Flat") {
+			type = "First Class Package";
+		}
+		getPrice(response, lb, oz, type);
 	}
+	function getPrice(response, lb, oz, type) {
+		var price = 0.0;
+		switch (type) {
+			case "First Class Letter: Stamped":
+				price = 1.0;
 
-	getPrice(response, lb, oz, type);
-}
-
-function getPrice(response, lb, oz, type) {
-	var price = 0.0;
-
-	switch (type) {
-		case "First Class Letter: Stamped":
-			price = 1.0;
-
-			break;
-		case "First Class Letter: Metered":
-			price = 2.0;
-			break;
-		case "First Class Envelope: Large Flat":
-			price = 3.0;
-			break;
-		case "default":
-			price = 4.0;
-
+				break;
+			case "First Class Letter: Metered":
+				price = 2.0;
+				break;
+			case "First Class Envelope: Large Flat":
+				price = 3.0;
+				break;
+			case "default":
+				price = 4.0;
+		}
+		const params = {
+			lb: lb,
+			oz: oz,
+			type: type,
+			price: price
+		};
+		response.render("postal/results", params);
 	}
-	const params = {
-		lb: lb,
-		oz: oz,
-		type: type,
-		price: price
+//end week 9 PROVE callback +  code
+
+
+//week 8 prove callback + code
+app.get("/math", function (req, res) {
+	// Controller
+	console.log("Received a request for the math page");
+	var params = {
+		result: getResults(req.query)
 	};
-	response.render("postal/results", params);
-}
+	res.render("math", params);
+});
+app.get("/math_service", function (req, res) {
+	console.log("Received a request for the math_service page");
+	res.render("math_service");
+});
+	//app.get("/math", function (req, res) {
+	function subtract(num1, num2) {
+		// console.log("Subtract" + num1 + ' ' + num2);
+		return parseInt(num1) - parseInt(num2);
+	}
+	function divide(num1, num2) {
+		// console.log("Divide" + num1 + ' ' + num2);
+		return parseInt(num1) / parseInt(num2);
+	}
+	function multiply(num1, num2) {
+		// console.log("Multiply" + num1 + ' ' + num2);
+		return parseInt(num1) * parseInt(num2);
+	}
+	function add(num1, num2) {
+		// console.log("Add" + num1 + ' ' + num2);
+		return parseInt(num1) + parseInt(num2);
+	}
+//end week 8 prove callback + code
+
+
+/*/*//*/*//*/*//*//*//*//**//*//*//*//*
+/* NOT SURE THE CODE BELOW WORKS OR NOT - FROM TEAM 4 TEACH SESSION
+		//code from team 4 teach session: week 10 teach callback + code
+		app.get("/person", function (req, res) {
+			// Controller
+			console.log("Received a request for the person page");
+			var params = {
+				result: getPerson(req.query)
+			};
+			res.render("person", params);
+		});
+
+		app.get("/services/getPerson", (req, res) => {
+
+			getPerson(req.query.id, res)
+				// return json object person
+			})
+			function callback(data, res) {
+				for (var i = 0; i < data.length; i++) {
+					console.log(data[i])
+				}
+			}
+			//app.get("/services/getPerson", (req, res)
+			function getPerson(id, res) {
+				var sql = "SELECT * FROM person_table WHERE person_id=" + id;
+				pool.query(sql, function(err, result) {
+					if (err) {
+						console.log("Error in Query" + err)
+					}
+					res.writeHead(200, {"Content-type": "application/json"})
+					res.write(JSON.stringify(result.rows))
+					res.end()
+				})
+			}
+		//end w10 teach callback + code: team 4 teach session 
+		*/
